96b92c716af8ec69774ba434b169c59c0d2afb5c
jagank1 <113684396+jagank1@users.noreply.github.com>
2022-10-03 11:17:35 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,6 +1,6 @@
 variable "vsphere_server" {
     description = "vsphere server for the environment - EXAMPLE: vcenter01.hosted.local"
-    default = "vcenter.test.local"
+    default = "tctc-vcsa01.swc.localdomain"
 }
 variable "vsphere_user" {
     description = "vsphere server for the environment - EXAMPLE: vsphereuser"
@@ -8,6 +8,7 @@ variable "vsphere_user" {
 }
 variable "vsphere_password" {
     description = "vsphere server password for the environment"
+    default = ""
 }
 variable "password" {
     description = "Root account password"
